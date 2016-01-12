936102a719337c1b826787ed20c93598fe35ea63
Lex <lexrus@gmail.com>
2016-01-12 18:29:16 +0800

    Load posts with Ajax.

---
+++ Static/scripts/main.js
@@ -0,0 +1,49 @@
+$(function() {
+  var location = window.history.location || window.location;
+  $(document).on('click', 'a.ajax', function() {
+    history.pushState(null, null, this.href);
+    load(this.href);
+    return false;
+  });
+
+  function load(href) {
+    NProgress.start();
+    var path = href.split('#');
+    if (path.count == 2) {
+      path = path[1];
+    } else {
+      path = 'posts';
+    }
+
+    $.getJSON('/api/v1/' + path, null, renderPosts)
+    .fail(function() {
+      // TODO: show 404.
+    })
+    .always(function() {
+      NProgress.done();
+    });
+  }
+
+  function renderPosts(json) {
+    var posts;
+    if (json['posts'] != undefined) {
+      posts = json['posts'];
+    } else if (json['id'] != undefined) {
+      posts = [json];
+    }
+    for (var i=0; i<posts.length; i++) {
+      var post = posts[i];
+      var postTemplate = $('#postTemplate').clone();
+      postTemplate.removeAttr('id');
+      postTemplate.removeAttr('style');
+      // postTemplate.find('.post-title a')
+      // TODO: Render author:
+      postTemplate.find('.post-meta').first().html(post['created']);
+      // TODO: Render markdown here:
+      postTemplate.find('.post-content').first().html(post['content']);
+      $('#posts').append(postTemplate);
+    }
+  }
+
+  load("posts");
+});
